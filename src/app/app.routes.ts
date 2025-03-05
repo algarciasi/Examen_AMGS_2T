831f5469890fd3cd0ab6e176d9212a138a6327a8
Alberto Garcia <albertomiguelgarciasimo@outlook.com>
2025-03-05 05:57:04 +0100

    creacion de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ProductlistComponent } from './pages/productlist/productlist.component';
+import { ProductoviewComponent } from './pages/productoview/productoview.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", pathMatch: "full", redirectTo: "home"},
+    {path: "home", component: ProductlistComponent},
+    {path:"producto/:_id", component: ProductoviewComponent},
+    {path:"**", redirectTo: "Error 404"}
+];
