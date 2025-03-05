a25c810762ad81ccebacd2710f21e4dd6ee20993
Alberto Garcia <albertomiguelgarciasimo@outlook.com>
2025-03-05 06:00:22 +0100

    edicion del service

--- src/app/pages/productlist/productlist.component.ts
+++ src/app/pages/productlist/productlist.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Producto } from '../../interface/producto';
+import { ProductosService } from '../../services/productos.service';
 
 @Component({
   selector: 'app-productlist',
@@ -8,5 +10,12 @@ import { Component } from '@angular/core';
   styleUrl: './productlist.component.css'
 })
 export class ProductlistComponent {
+
+  arrProductos: Producto[];
+  productoService = inject(ProductosService);
+
+  constructor(){
+    this.arrProductos=[];
+  }
 
 }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Producto } from '../interface/producto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
 
+  httpClient = inject(HttpClient);
+  private baseUrl : string = 'https://peticiones.online/api/products';
+  insertObservable: any;
+
   constructor() { }
+
+  getAllWithObservables(): Observable<Producto[]>{
+    return this.httpClient.get<Producto[]>(this.baseUrl);
+  }
+
+  getByIdWithObservable(_id: string): Observable<Producto>{
+    return this.httpClient.get<Producto>(`${this.baseUrl}/${_id}`);
+  }
 }
