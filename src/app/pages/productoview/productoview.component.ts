af8c09b4e00a7e2924f7c4ca0b5d3831e55152ad
Alberto Garcia <albertomiguelgarciasimo@outlook.com>
2025-03-05 05:50:05 +0100

    componentes creados

---
+++ src/app/pages/productoview/productoview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-productoview',
+  standalone: true,
+  imports: [],
+  templateUrl: './productoview.component.html',
+  styleUrl: './productoview.component.css'
+})
+export class ProductoviewComponent {
+
+}
